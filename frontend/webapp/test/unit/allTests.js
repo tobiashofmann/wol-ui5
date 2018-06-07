9b29fcdda5c2dc7c1bc04b5ee7279a48d1ecff78
tobiashofmann <hofmann.tobias@outlook.com>
2018-06-07 11:08:10 +0200

    unit tests framework
    Change-Id: I9df3c5952a470251f2f9cbd55f06f94cad4ba1fa

---
+++ frontend/webapp/test/unit/allTests.js
@@ -0,0 +1,9 @@
+sap.ui.define([
+	"test/unit/model/Formatter",
+	"test/unit/model/DeviceModel",
+	"test/unit/model/BusyDialogModel",
+	"test/unit/model/ServerModel",
+	"test/unit/model/Helper"
+], function() {
+	"use strict";
+});
